d71200f04c461613615fbcfb6f8912da40a92465
Min Lee <minlee@Zipcoder04s-MacBook-Pro.local>
2016-05-23 18:31:48 -0400

    String Lab done without UML

--- src/main/java/driverslicense/Restriction.java
+++
@@ -1,8 +0,0 @@
-package driverslicense;
-
-/**
- * Created by davidginzberg on 5/20/16.
- */
-public enum Restriction {
-    GLASSES, DUAL_MIRRORS, AUTOMATIC_TRANSMISSION, SPECIAL_EQUIPMENT, DAYTIME_DRIVING, VISUAL_IMPAIRMENT
-}
